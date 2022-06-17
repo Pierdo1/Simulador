48b48b708d466c4728e06bd2c866f4804967a263
Agustin <69096730+Pierdo1@users.noreply.github.com>
2022-06-16 21:36:00 -0300

    Add files via upload

---
+++ SimuladorV2/js/variables.js
@@ -0,0 +1,12 @@
+const cpu = ['Ryzen 7', 'Intel Core i7']
+const gpu = ['Nvidia RTX2060', 'Radeon RX570']
+const ram = ['HyperX 16GB', 'Msi 8GB']
+const mother = ['Aorus X470', 'MSI X570']
+
+const carrito = []
+
+const titulo = document.getElementById("titulo")
+const slogan = document.getElementById("slogan")
+
+const Procesador = document.getElementById("Procesador", 'placaDeVideo', 'memoriaRam', 'placaMadre') 
+const listadoCarrito = document.getElementById("listadoCarrito")
